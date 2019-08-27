05c30d584597e3ef060cb3bacffc0afa5f9affd1
haider <haider.shah@hotmail.com>
2019-08-27 05:07:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,7 @@
-$status=0
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
-  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
-  return $status
 end
 
 mario()
